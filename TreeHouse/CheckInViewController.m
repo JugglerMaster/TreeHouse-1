4c8ac19043b0573a1079ba7f94c2154fcd649624
Christopher Dadisman <dadisman@DadismanMBP.local>
2013-11-04 15:07:52 -0600

    This is now the Correct files

--- TreeHouse/CheckInViewController.m
+++ TreeHouse/CheckInViewController.m
@@ -35,4 +35,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) viewWillAppear:(BOOL)animated
+{
+    
+}
+-(void) viewDidAppear:(BOOL)animated
+{
+    
+}
+-(void) viewWillDisappear:(BOOL)animated
+{
+    
+}
+-(void)viewDidDisappear:(BOOL)animated
+{
+    
+}
 @end

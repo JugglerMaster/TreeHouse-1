70422817c6c3747819c823d010b605e3b8987a1c
Christopher Dadisman <dadisman@DadismanMBP.local>
2013-11-04 14:56:11 -0600

    test

---
+++ TreeHouse/TreeHouse/CheckInViewController.m
@@ -0,0 +1,38 @@
+//
+//  CheckInViewController.m
+//  TreeHouse
+//
+//  Created by Christopher Dadisman on 11/4/13.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import "CheckInViewController.h"
+
+@interface CheckInViewController ()
+
+@end
+
+@implementation CheckInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
